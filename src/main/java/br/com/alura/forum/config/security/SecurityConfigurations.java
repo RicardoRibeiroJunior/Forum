c91261dd5008a09f5bc11799b31c13ff948ed29c
Ricardo Ribeiro <ricardoribeiro.j7@gmail.com>
2022-08-22 16:57:44 -0300

    feature-8: implemented token com JWT.

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -17,11 +20,18 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private AutenticacaoService autenticacaoService;
 	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
 	//Usado para configurações de autenticação
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
+	
 	
 	//Usado para configurações de autorização das URL´s
 	@Override
@@ -30,9 +40,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll()//liberando URL publica para listar.
 			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()//liberando URL para detalhar um tópico (*).
+			.antMatchers(HttpMethod.POST, "/auth").permitAll()
 			.anyRequest().authenticated()//Qualquer outra requisição tem que está autenticado.
-			.and().formLogin();
+			.and().csrf().disable()//desabilita a segurança csrf
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//informa que será usado token na comunicação
 	}
+	
+	
 	
 	//Usado para configurações de recursos estáticos (Acesso as js, css, img)
 	@Override
